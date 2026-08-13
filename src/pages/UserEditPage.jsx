57a4a2af807b82b050ee1e76d85fb81f9ea7c3d6
IcaroMatheusz <icaromatheus741@gmail.com>
2026-08-12 21:31:27 -0300

    criado e configurado a página de criação do usuário

--- src/pages/UserEditPage.jsx
+++ src/pages/UserEditPage.jsx
@@ -1,12 +1,86 @@
+import { useState } from "react";
 import MainLayout from "../components/MainLayout";
 
 function UserEditPage() {
+  const [pfp, setPfp] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmpassword, setConfirmPassword] = useState("");
+  const [error, setError] = useState("");
+
+  function handleEdit(e) {
+    e.preventDefault();
+
+    setError("");
+
+    if (confirmpassword !== password) {
+      setError("A senha não é a mesma!");
+      return;
+    }
+  }
+
   return (
     <>
       <MainLayout title="Configuração do Usuário">
+        <main className="flex justify-center items-center text-white">
+          <div className="w-2xl mx-auto bg-slate-800 rounded-2xl border border-gray-50 overflow-hhtmlForden my-8">
+            <form
+              onSubmit={handleEdit}
+              className="px-18 py-6 space-y-6 flex flex-col mb-8 text-white"
+            >
+              <span className="text-red-500">{error}</span>
 
-        <main>
-          <h1>UserEdit Component will be there</h1>
+              <h1 className="font-bold">Editar Perfil</h1>
+
+              {pfp ? (
+                <img src={pfp} alt="fotodeperfil" className="w-40 rounded-xl" />
+              ) : (
+                <h3>Sem foto de Perfil</h3>
+              )}
+
+              <input //PREVIEW DA PFP
+                type="file"
+                accept="image/*"
+                onChange={(e) => {
+                  const file = e.target.files[0];
+
+                  if (file) {
+                    setPfp(URL.createObjectURL(file)); 
+                  }
+                }}
+                className="w-full border border-slate-500 rounded-lg p-3"
+              />
+
+              <label htmlFor="username" className="">
+                Nome de Usuário:
+              </label>
+              <input
+                onChange={(e) => setUsername(e.target.value)}
+                type="text"
+                id="username"
+                placeholder="Digite seu nome de usuário"
+                className="w-full border border-slate-500 rounded-lg p-3"
+              />
+
+              <label htmlFor="password">Nova Senha:</label>
+              <input
+                onChange={(e) => setPassword(e.target.value)}
+                type="password"
+                id="password"
+                placeholder="Digite sua nova senha"
+                className="w-full border border-slate-500 rounded-lg p-3"
+              />
+
+              <label htmlFor="confirmar-senha">Confirmar Senha:</label>
+              <input
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                type="password"
+                id="confirmar-senha"
+                placeholder="Digite sua nova senha"
+                className="w-full border border-slate-500 rounded-lg p-3"
+              />
+            </form>
+          </div>
         </main>
       </MainLayout>
     </>
